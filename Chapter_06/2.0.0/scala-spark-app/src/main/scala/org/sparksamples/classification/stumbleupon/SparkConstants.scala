a755303eb4a7cfad711b2d70aa53f253794dcd42
xingxingt <1039380441@qq.com>
2019-07-23 16:20:34 +0800

    Update SparkConstants.scala

--- Chapter_06/2.0.0/scala-spark-app/src/main/scala/org/sparksamples/classification/stumbleupon/SparkConstants.scala
+++ Chapter_06/2.0.0/scala-spark-app/src/main/scala/org/sparksamples/classification/stumbleupon/SparkConstants.scala
@@ -6,5 +6,10 @@ package org.sparksamples.classification.stumbleupon
 object SparkConstants {
 
   val SparkMaster = "local[1]"
+ 
+ 
+  val path = "E:\\dev\\ideaWorkSpaces\\spark-ml-branch-ed2\\Chapter_06\\2.0.0\\scala-spark" +
+    "-app\\src\\main\\scala\\org\\sparksamples\\classification\\dataset"
+
 
 }
