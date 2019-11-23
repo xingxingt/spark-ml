310a39af811a852d0a778f1dbdd9595c6436dd6c
xingxingt <1039380441@qq.com>
2019-11-23 09:37:28 +0800

    本地案例

--- Chapter_06/1.6.2/scala-spark-app/src/main/scala/org/sparksamples/classification/DataPersistenceApp.scala
+++ Chapter_06/1.6.2/scala-spark-app/src/main/scala/org/sparksamples/classification/DataPersistenceApp.scala
@@ -4,28 +4,30 @@ import org.apache.spark.SparkContext
 import org.sparksamples.classification.stumbleupon.SparkConstants
 
 /**
-	* @author Rajdeep dua
-	*         
-	*/
+  * @author Rajdeep dua
+  *
+  */
 
 object DataPersistenceApp {
 
-	def main(args: Array[String]) {
-		val sc = new SparkContext("local[1]", "Classification")
+  def main(args: Array[String]) {
+    val sc = new SparkContext("local[1]", "Classification")
 
-		// get StumbleUpon dataset 'https://www.kaggle.com/c/stumbleupon'
-		val records = sc.textFile(SparkConstants.PATH + "data/train_noheader.tsv").map(line => line.split("\t"))
+    // get StumbleUpon dataset 'https://www.kaggle.com/c/stumbleupon'
+    val records = sc.textFile(SparkConstants.PATH + "stumbleupon/train_noheader.tsv").map(line => line.split("\t"))
+
+    records.first().foreach(println)
 
     val data_persistent = records.map { r =>
       val trimmed = r.map(_.replaceAll("\"", ""))
       val label = trimmed(r.size - 1).toInt
       val features = trimmed.slice(4, r.size - 1).map(d => if (d == "?") 0.0 else d.toDouble)
       val len = features.size.toInt
-      val len_2 = math.floor(len/2).toInt
-      val x = features.slice(0,len_2)
+      val len_2 = math.floor(len / 2).toInt
+      val x = features.slice(0, len_2)
 
-      val y = features.slice(len_2 -1 ,len )
-      var i=0
+      val y = features.slice(len_2 - 1, len)
+      var i = 0
       var sum_x = 0.0
       var sum_y = 0.0
       while (i < x.length) {
@@ -38,25 +40,25 @@ object DataPersistenceApp {
         i += 1
       }
       if (sum_y != 0.0) {
-        if(sum_x != 0.0) {
+        if (sum_x != 0.0) {
           math.log(sum_x) + "," + math.log(sum_y)
-        }else {
+        } else {
           sum_x + "," + math.log(sum_y)
         }
 
-      }else {
-        if(sum_x != 0.0) {
+      } else {
+        if (sum_x != 0.0) {
           math.log(sum_x) + "," + 0.0
-        }else {
+        } else {
           sum_x + "," + 0.0
         }
       }
 
     }
     val dataone = data_persistent.first()
-    data_persistent.saveAsTextFile(SparkConstants.PATH + "/results/raw-input-log")
-		sc.stop()
+    data_persistent.saveAsTextFile(SparkConstants.PATH + "results/raw-input-log")
+    sc.stop()
 
-	}
+  }
 
 }
